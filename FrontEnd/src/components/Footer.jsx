c45db5de7bfd442838a1e9cac04d47e801110556
codabishekk <abishekkc@gmail..com>
2026-09-10 14:46:20 +0530

    feat: add component-specific CSS modules and implement initial Footer layout

--- FrontEnd/src/components/Footer.jsx
+++ FrontEnd/src/components/Footer.jsx
@@ -62,7 +62,7 @@ const Footer = () => {
             </div>
             <div className="footer-bottom">
                 <div className="container">
-                    <p>&copy; 2026 Sholash Life Science. Developed by AIM Universe.</p>
+                    {/*<p>&copy; 2026 Sholash Life Science. Developed by AIM Universe.</p>*/}
                 </div>
             </div>
         </footer>
